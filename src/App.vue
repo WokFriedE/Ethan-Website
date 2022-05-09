5c6eb44b1fa1ade3310ce3e936cf67bc3a199965
WokFriedE <47959663+WokFriedE@users.noreply.github.com>
2022-05-09 18:38:41 -0400

    Start card-item

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,8 @@
     <div style="border-bottom: 2px solid rgba(0, 0, 0, 0.2); max-width: 75%; margin-inline: 10%;" />
     <br />
 
+    <CardItemVue />
+
     <!-- The footer segment -->
     <div style="height: 2rem;  background: linear-gradient(0deg, dimgrey 20%, rgba(255, 255, 255,0) 60%); " />
     <footerVue />
@@ -33,6 +35,7 @@ import profileMainVue from "./components/Profile-Main.vue";
 import projectsVue from "./components/Projects-Main.vue";
 import boxListingsVue from "./components/Box-Listings.vue";
 import footerVue from "./components/Footer-Main.vue";
+import CardItemVue from "./components/Card-Item.vue";
 
 import skills from "./data/skills.json";
 import languages from "./data/languages.json";
@@ -44,6 +47,7 @@ export default {
     projectsVue,
     boxListingsVue,
     footerVue,
+    CardItemVue
   },
 
   data() {
---
+++ src/components/Card-Item.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <div class="card-front">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'description', 'img', 'start', 'end', 'link'],
+};
+</script>
+
+<style>
+.card {
+    background-color: var(--nav-background);
+    border-radius: 4px;
+    box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.2);
+    height: 250px;
+    width: 250px;
+}
+</style>
